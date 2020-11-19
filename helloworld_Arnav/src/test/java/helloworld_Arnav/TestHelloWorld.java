7c67e2f6f91cb78e054c53066f3c63079fbd2a23
Chauch <Chauch@DESKTOP-O35O75L.mshome.net>
2020-11-19 20:49:39 +0530

    one commit only

---
+++ helloworld_Arnav/src/test/java/helloworld_Arnav/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld_Arnav;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testHelloWorld() {
+		
+		System.out.println("...hello there...");
+	}
+
+}
